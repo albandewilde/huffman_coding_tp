6faf69074f9a3a7442fa1145ec9bbd66cfeefd34
Arthur <arthur.cheng@signature-code.com>
2020-10-09 15:41:29 +0200

    count char

--- count_char.py
+++ count_char.py
@@ -1,10 +1,4 @@
 from to_bin import read_file
 
 def count_char( path ):
-    content = read_file( path )
-    
-    count = 0
-    for char in content:
-        count += 1
-    
-    print(count)
+    return len( read_file( path ) )
