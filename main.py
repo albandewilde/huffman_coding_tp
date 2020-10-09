422558b132d2b2311140070125fc875fc75fde21
albandewilde <dewilde@et.intechinfo.fr>
2020-10-09 15:44:25 +0200

    print chars in main

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python3
+
 from to_bin import encode_file_to_bites
+from count_char import count_char
 
 # Encode the file as a binaire
 encode_file_to_bites("alice.txt", "output.txt")
 
-
+print("char in file alice.txt: ", count_char("alice.txt"))
+print("char in file output.txt: ", count_char("output.txt"))
