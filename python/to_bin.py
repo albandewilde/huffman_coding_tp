f5a593313660f9972e15a3adb6b893bd9b022d23
Kimor <agopian@intechinfo.fr>
2020-10-16 11:37:44 +0200

    once again remove unused meth

--- python/to_bin.py
+++ python/to_bin.py
@@ -1,8 +1,3 @@
-def str_to_bin(text):
-    """Convert a string to a binaire charactes as at least 8 bits"""
-    for char in text:
-        yield "{:08b}".format(ord(char))
-
 def read_file(filepath):
     """Read file content and return his string"""
     with open(filepath, "r") as fle:
