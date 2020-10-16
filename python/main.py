16c0f73405779de51d2b02329cd2434bb08579a2
albandewilde <dewilde@et.intechinfo.fr>
2020-10-16 14:18:07 +0200

    Add time of things

--- python/main.py
+++ python/main.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
+import datetime
+
 from to_bin import read_file, char_to_bytes, write_to_file
 from count_char import count_char
 from occurence import get_occurence, text_to_bin
 from huffman_tree import sort_occurence_dico, to_dict, to_tree, dico_to_file
 
 input_file: str = 'alice.txt'
-alice = read_file(input_file)
-nb_char_in_alice = count_char(alice)
 output_classic_bytes = 'py_encoded_alice.txt'
 output_huffman = 'py_alice_huffman.txt'
 output_huffman_dict = 'py_huffman_dict.txt'
@@ -22,11 +22,28 @@ def browse_txt():
     write_to_file(encoded, output_classic_bytes)
     return occurence
 
+begin = datetime.datetime.now()
+alice = read_file(input_file)
+print("Reading file: ", datetime.datetime.now() - begin)
+
+begin = datetime.datetime.now()
+nb_char_in_alice = count_char(alice)
 occurence = browse_txt()
+print("Counting occurence: ", datetime.datetime.now() - begin)
+
+begin = datetime.datetime.now()
 sorted_occurence = sort_occurence_dico(occurence)
 huffman = to_tree(sorted_occurence)
+print("Making huffman tree: ", datetime.datetime.now() - begin)
+
+begin = datetime.datetime.now()
 huffman_dict = to_dict(huffman)
+print("Encoding the text: ", datetime.datetime.now() - begin)
+
+begin = datetime.datetime.now()
 dico_to_file(huffman_dict, output_huffman_dict)
+print("Writing to file: ", datetime.datetime.now() - begin)
+
 text_to_bin(alice, output_huffman_dict, output_huffman)
 
 
