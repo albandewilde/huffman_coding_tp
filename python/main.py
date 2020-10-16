3a5806877a3e6aa2d710b662756d1485e258b6d4
Kimor <agopian@intechinfo.fr>
2020-10-16 11:46:42 +0200

    a bit of organisation

--- python/main.py
+++ python/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from to_bin import encode_file_to_bites, read_file, char_to_bytes, write_to_file
+from to_bin import read_file, char_to_bytes, write_to_file
 from count_char import count_char
 from occurence import get_occurence, text_to_bin
 from huffman_tree import sort_occurence_dico, to_dict, to_tree, dico_to_file
@@ -8,9 +8,9 @@ from huffman_tree import sort_occurence_dico, to_dict, to_tree, dico_to_file
 input_file: str = 'alice.txt'
 alice = read_file(input_file)
 nb_char_in_alice = count_char(alice)
-output_classic_bytes = 'encoded_alice.txt'
-output_huffman = 'alice_huffman.txt'
-output_huffman_dict = 'huffman_dict.txt'
+output_classic_bytes = 'py_encoded_alice.txt'
+output_huffman = 'py_alice_huffman.txt'
+output_huffman_dict = 'py_huffman_dict.txt'
 
 def browse_txt():
     encoded = ""
@@ -28,9 +28,6 @@ huffman = to_tree(sorted_occurence)
 huffman_dict = to_dict(huffman)
 dico_to_file(huffman_dict, output_huffman_dict)
 text_to_bin(alice, output_huffman_dict, output_huffman)
-
-
-
 
 
 # Just to see result
